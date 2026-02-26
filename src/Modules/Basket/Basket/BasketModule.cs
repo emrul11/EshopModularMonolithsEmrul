d87191328725c59b9400e6d6e57343692c356b31
emrul11 <emrulkabir111@gmail.com>
2026-02-26 15:58:08 +0600

    Module created for DI

---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket
+{
+    public static class BasketModule
+    {
+        public static IServiceCollection AddBasketModule(this IServiceCollection services, 
+            IConfiguration configuration) 
+        {
+            return services;
+        }
+    }
+}
